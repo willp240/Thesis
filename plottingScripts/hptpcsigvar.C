ac30b29f4396786ae0d60fad8c9b02697193d74c
William Parker <wparker@lx02.hep.ph.ic.ac.uk>
2020-05-04 00:14:30 +0100

    update hptpc sig var plots

--- plottingScripts/hptpcsigvar.C
+++ plottingScripts/hptpcsigvar.C
@@ -12,19 +12,22 @@
 #include <algorithm>
 
 
-void hptpcsigvar(std::string file0) {
+void hptpcsigvar() {
 
   gStyle->SetPalette(51);
 
   TCanvas *c = new TCanvas("canv", "canv", 1580, 1080);
-  TFile *file_ = TFile::Open(file0.c_str());
- 
+  TFile *filemu = TFile::Open("inputFiles/STV/RFG_PmuTmu.Spectra.root");
+  TFile *filep = TFile::Open("inputFiles/STV/RFGMAQE_PpTmu.Spectra.root"); 
+  TFile *filepi = TFile::Open("inputFiles/STV/HPTPC_Ppion_costheta_NormFakeData.root");
+  TFile *filept = TFile::Open("inputFiles/STV/ND280_MixedSelections.Spectra.root");
+
   c->SetTopMargin(0.05);
   c->SetBottomMargin(0.14);
   c->SetLeftMargin(0.1);
   c->SetRightMargin(0.16);
   
-  std::string samp[7] = {"cc0pi0p", "cc0pi1p", "cc0piNp", "cc1pi0p", "cc1pi1p", "cc1piNp", "ccOther"};
+  std::string samp[7] = {"cc0pi0p", "cc0pi1p", "cc0piNp", "nu_cc1pi0p_1", "cc1pi1p", "cc1piNp", "ccOther"};
 
   std::string xtitle[7] = {"p_{#mu} (MeV)", "p_{p} (MeV)", "p_{p} (MeV)", "p_{#pi} (MeV)", "#delta#it{p}_{T} (MeV)","p_{p} (MeV)", "p_{#mu} (MeV)"};
   std::string ytitle[7] = {"cos#theta_{#mu}", "cos#theta_{#mu}", "cos#theta_{#mu}", "cos#theta_{#mu}", "cos#theta_{#mu}", "cos#theta_{#mu}", "cos#theta_{#mu}"};
@@ -33,8 +36,15 @@ void hptpcsigvar(std::string file0) {
 
     std::string name = samp[s];
 
-    TH2D *hist = (TH2D*) file_->Get(name.c_str())->Clone();
-
+    if(s==0 || s==6)
+      TH2D *hist = (TH2D*) filemu->Get(name.c_str())->Clone();
+    if(s==1 || s== 2 || s==5)
+      TH2D *hist = (TH2D*) filep->Get(name.c_str())->Clone();
+    if(s==3)
+      TH2D *hist = (TH2D*) filepi->Get(name.c_str())->Clone();
+    if(s==4)
+      TH2D *hist = (TH2D*) filept->Get(name.c_str())->Clone();
+    
     hist->SetTitle("");
     hist->GetYaxis()->SetTitleOffset(0.85);
     hist->GetXaxis()->SetTitleOffset(1.0);
@@ -47,12 +57,15 @@ void hptpcsigvar(std::string file0) {
     hist->GetXaxis()->SetTitle(xtitle[s].c_str());
     hist->GetYaxis()->SetTitle(ytitle[s].c_str());
     hist->GetZaxis()->SetTitle("Events");
-    palette = (TPaletteAxis*) hist->GetListOfFunctions()->FindObject("palette");
-    palette->SetX1NDC(0.85);
-    palette->SetX2NDC(0.90);
-    palette->SetY1NDC(0.14);
-    palette->SetY2NDC(0.95);
+    if(s!=3){
+      palette = (TPaletteAxis*) hist->GetListOfFunctions()->FindObject("palette");
+      palette->SetX1NDC(0.85);
+      palette->SetX2NDC(0.90);
+      palette->SetY1NDC(0.14);
+      palette->SetY2NDC(0.95);
+    }
     hist->Draw("colz");
+    //    std::cout << "print to " << "hptpc_sigvar_" << std::endl;
     c->Print((std::string("hptpc_sigvar_")+name+std::string(".pdf")).c_str());
     std::cout << "got " << name << std::endl;
   }
